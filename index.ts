ac582720aa4f5396b253474d7164a74243f547e1
nicoandresr <nicoandres.rodriguez@gmail.com>
2016-10-23 07:08:35 -0500

    Connect to mongo db

--- index.ts
+++ index.ts
@@ -6,16 +6,30 @@ module Serfid {
     export class Server {
         static consoleLogStartMsg: string = "App listening on port: ";
         private _app: express.Application;
-        private _uriMongo: string = "mongodb://nico:'S3rf1d'@ds061246.mlab.com:61246/serfid-db";
+        private _uriMongo: string = "mongodb://user:pass@ds061246.mlab.com:61246/serfid-db";
+        private _port: number;
 
-        constructor(port: number){
-                this._app = express();
-                this.configureExpressServer(port);
+        constructor(port: number) {
+            this._port = port;
+            this._app = express();
+            mongodb.MongoClient.connect(this._uriMongo, (error, db) => {
+                assert.equal(null, error);
+                this.mongoConnectHandler(db);
+            });
         }
 
-        private configureExpressServer(port: number):void {
-                this._app.get("/", (req, res) => { res.send("hola mundo") });
-                this._app.listen(port, () => { console.log(Server.consoleLogStartMsg + port) });
+        private mongoConnectHandler(db: any): void {
+            db.collection("devices").find().toArray((error, docs) => {
+                assert.equal(null, error);
+                let devices: string;
+                devices = JSON.stringify(docs);
+                this.configureExpressServer(devices);
+            });
+        }
+
+        private configureExpressServer(devices: string):void {
+                this._app.get("/", (req, res) => { res.send(devices) });
+                this._app.listen(this._port, () => { console.log(Server.consoleLogStartMsg + this._port) });
         }
     }
 }
