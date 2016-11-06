5b96404eb30e10c2af34d903f3dd10c37d000bc1
nicoandresr <nicoandres.rodriguez@gmail.com>
2016-11-05 20:51:31 -0500

    Organize folders and set up express server

---
+++ Infrastructure/router.ts
@@ -0,0 +1,24 @@
+import * as express from "express";
+import * as assert from "assert";
+
+export class Router {
+
+    constructor(private _db: any) {}
+
+    public configRoutes(): express.Router {
+        let router = express.Router();
+
+        this._db.collection("devices").find().toArray((error, docs) => {
+            assert.equal(null, error);
+
+            let devices: string;
+            devices = JSON.stringify(docs);
+
+            router.get("/", (req, res) => {
+                res.send(devices);
+            });
+        });
+
+        return router;
+    }
+}
--- index.ts
+++ index.ts
@@ -1,37 +1,38 @@
 import * as express from "express";
 import * as assert from "assert";
 import * as mongodb from "mongodb";
+import { Router } from "./Infrastructure/router";
 
-module Serfid {
-    export class Server {
-        static consoleLogStartMsg: string = "App listening on port: ";
-        private _app: express.Application;
-        private _uriMongo: string = "mongodb://user:pass@ds061246.mlab.com:61246/serfid-db";
-        private _port: number;
+export class SerfidServer {
 
-        constructor(port: number) {
-            this._port = port;
-            this._app = express();
-            mongodb.MongoClient.connect(this._uriMongo, (error, db) => {
-                assert.equal(null, error);
-                this.mongoConnectHandler(db);
-            });
-        }
+    private _startMsg: string = "App listening on port: ";
+    private _uriMongo: string = "mongodb://nico:'S3rf1d'@ds061246.mlab.com:61246/serfid-db";
+    private _app: express.Application;
+    private _port: number;
+    private _router: express.Router;
 
-        private mongoConnectHandler(db: any): void {
-            db.collection("devices").find().toArray((error, docs) => {
-                assert.equal(null, error);
-                let devices: string;
-                devices = JSON.stringify(docs);
-                this.configureExpressServer(devices);
-            });
-        }
+    constructor(port: number) {
+        this._port = port;
+        this._startMsg += port;
+        this._app = express();
 
-        private configureExpressServer(devices: string):void {
-                this._app.get("/", (req, res) => { res.send(devices) });
-                this._app.listen(this._port, () => { console.log(Server.consoleLogStartMsg + this._port) });
-        }
+        this.startSerfidServer();
+    }
+
+    private startSerfidServer(): void {
+        mongodb.MongoClient.connect(this._uriMongo, (error, db) => {
+            assert.equal(null, error);
+            let serfidRouter = new Router(db);
+            this._router = serfidRouter.configRoutes();
+            this.configureExpressServer();
+        });
+    }
+
+
+    private configureExpressServer(): void {
+        this._app.use("/", this._router);
+        this._app.listen(this._port, () => { console.log(this._startMsg) });
     }
 }
 
-new Serfid.Server(7000);
+new SerfidServer(7000);
